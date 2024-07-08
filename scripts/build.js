162b3c01f30176be12d6829857292df1c9e52228
Samodya Abeysiriwardane <git@sransara.com>
2024-07-07 20:35:23 -0500

    build instead of publish

--- scripts/build.js
+++ scripts/build.js
@@ -1,11 +1,3 @@
-// node script to
-// 1. change to project root directory
-// 2. run `npm run build`
-// 3. change to `dist` directory
-// 4. copy `package.json` file to `dist` directory
-// 4. copy `README.md` and `LICENSE` files to `dist` directory
-// 5. run `npm publish` in `dist` directory
-
 import { execSync } from 'node:child_process';
 import { copyFileSync } from 'node:fs';
 import { dirname, resolve } from 'node:path';
@@ -16,11 +8,10 @@ const projectRoot = resolve(__dirname, '..');
 const distDir = resolve(projectRoot, 'dist');
 
 process.chdir(projectRoot);
-execSync('npm run build');
-process.chdir(distDir);
+execSync('node ./scripts/compile-rb.js ./src/patches/');
+execSync('npm run tsc');
 for (const file of ['package.json', 'README.md', 'LICENSE']) {
     const projectFile = resolve(projectRoot, file);
     const distFile = resolve(distDir, file);
     copyFileSync(projectFile, distFile);
 }
-execSync('npm publish --access=public');
